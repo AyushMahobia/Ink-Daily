52470cdea5e5427dff57c30a787e314d3b24b350
Ayush <ayushmahobia01@gmail.com>
2023-03-12 20:48:56 +0530

    adding validation

--- backend/models/User.js
+++ backend/models/User.js
@@ -19,5 +19,6 @@ const UserSchema = new mongoose.Schema({
         default: Date.now
     }
 })
-
-module.exports = mongoose.model('user', UserSchema);
+const User = mongoose.model('user', UserSchema);
+User.createIndexes();
+module.exports = User
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const User = require('../models/User')
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
-router.post('/', (req, res)=>{
-    console.log(req.body);
-    const user = User(req.body);
-    user.save();
-    res.send(req.body);
+router.post('/',[
+    body('name',"Minium length should be 3").isLength({min: 3}),
+    body('email',"Enter a valid email").isEmail(),
+    body('password',"Password must be atleast of 8 characters").isLength({min: 8})
+], (req, res)=>{
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+      }).then(user => res.json(user))
+      .catch(err=>{console.log("Enter the uniqe value for email")
+            res.json({error:"Enter unique email address", msg:err.message})
+        })
 })
 
 module.exports = router
