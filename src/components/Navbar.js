1b1a8b230cf1452c5082e69506f572303279421b
Ayush <ayushmahobia01@gmail.com>
2023-06-15 17:22:55 +0530

    .

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ export default function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
             <div className="container-fluid">
-                <Link className="navbar-brand mx-2" to="/">Ink-Daily</Link>
+                <Link className="navbar-brand mx-2" to="/">iNotebook</Link>
                 {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button> */}
